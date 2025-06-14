e68f2b38f3bbf3e619150b27d733c9ea505048c3
Jaish Chimnani <jaish.chimnani@geminisolutions.com>
2025-06-14 16:20:58 +0530

    fixed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // ... other configurations
+  darkMode: 'class', // <--- Add this line
+  theme: {
+    extend: {
+      // ... your existing theme extensions
+    },
+  },
+  plugins: [],
+}
